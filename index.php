88b05d8d965f45ccddc070e4813fc6badde313de
ananya63070182 <73657454+ananya63070182@users.noreply.github.com>
2020-11-06 18:12:19 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Web</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="row">
+                    <img src="img/afteryou/desert2.jpg" class="img-fluid" style="padding: 1px; height: 900px;"/>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="row">
+                    <img src="img/afteryou/desert1.jpg" class="img-fluid" style="padding: 1px; width: 300px; height: 300px;"/>
+                </div>
+                <div class="row">
+                    <img src="img/afteryou/desert4.jpg" class="img-fluid" style="padding: 1px; width: 300px; height: 300px;"/>
+                </div>
+                <div class="row">
+                    <img src="img/afteryou/desert9.jpg" class="img-fluid" style="padding: 1px; width: 300px; height: 300px;"/>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="row">
+                    <img src="img/afteryou/desert12.jpg" class="img-fluid" style="padding: 1px; width: 300px; height: 600px;""/>
+                </div>
+                <div class="row">
+                    <img src="img/afteryou/desert11.jpg" class="img-fluid" style="padding: 1px; width: 300px; height: 300px;"/>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
